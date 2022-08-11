3319fd32c6e1a84e8718c48a9ff5113dae2e20c7
tolzydrill <37539097+tolzydrill@users.noreply.github.com>
2022-08-11 23:24:42 +0100

    initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBADeg6djcsQMvpWPrI-pOrEyhr0RrZW5U",
-  authDomain: "todo-app-react-7bf11.firebaseapp.com",
-  projectId: "todo-app-react-7bf11",
-  storageBucket: "todo-app-react-7bf11.appspot.com",
-  messagingSenderId: "743991304307",
-  appId: "1:743991304307:web:61d2175b3675b344fe8a06",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 // Initialize Firebase
